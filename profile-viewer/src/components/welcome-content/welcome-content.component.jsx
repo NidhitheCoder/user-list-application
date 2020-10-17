1df0610c4cbc591761ea43602c596da315a6e426
NidhitheCoder <nidhpv@gmail.com>
2020-10-17 13:34:42 +0530

    Welcome component created

---
+++ profile-viewer/src/components/welcome-content/welcome-content.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./welcome-component.styles.scss";
+
+const Welcome = () => (
+  <div className="welcome">
+   <h2>Welcome to Application</h2>
+   <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id
+   iaculis leo. Interdum et malesuada fames ac ante ipsum primis in
+   faucibus. Phasellus tempor luctus sem vel imperdiet. Donec gravida
+   consequat tortor sit amet molestie. Aenean accumsan, lacus non
+   pharetra viverra, arcu tortor vulputate urna, eu volutpat lorem leo eu
+   quam. Maecenas sodales viverra justo sit amet consequat.</p>
+  </div>
+);
+
+export default Welcome;
--- profile-viewer/src/redux/usersData/users-data.selectors.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from "reselect";
-
-const selectCurrentUser = state => state.UsersData;
-
-export const getCurrentUser = createSelector(
-  selectCurrentUser,
-  UsersData => 
-  console.log(UsersData)
-);
