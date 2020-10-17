5806566234b22c9949b1afa65789965c2a8409ff
NidhitheCoder <nidhpv@gmail.com>
2020-10-17 11:44:54 +0530

    Search keywork added to redux  store

--- profile-viewer/src/components/search/search.component.jsx
+++ profile-viewer/src/components/search/search.component.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React from "react";
 import InputBase from "@material-ui/core/InputBase";
+import { connect } from "react-redux";
 
-import './search.styles.scss';
+import "./search.styles.scss";
+import { addSearchToStore } from "../../redux/usersData/users-data.action";
 
-export const SearchInput = () => (
+export const SearchInput = ({ AddSearch }) => {
+  const handleChange = e => {
+    AddSearch(e.target.value);
+  };
+
+  return (
     <div className="search-container">
-    <InputBase placeholder="Search Name..." className="search"></InputBase>
+      <InputBase
+        placeholder="Search Name..."
+        onChange={handleChange}
+        className="search"
+      ></InputBase>
     </div>
-);
+  );
+};
 
-export default SearchInput;
+const mapDispatchToProps = dispatch => ({
+  AddSearch: keyword => {
+    return dispatch(addSearchToStore(keyword));
+  }
+});
+
+export default connect(null, mapDispatchToProps)(SearchInput);
--- profile-viewer/src/redux/usersData/users-data.types.js
+++ profile-viewer/src/redux/usersData/users-data.types.js
@@ -1,6 +1,7 @@
-const usersDataActionTypes ={
-    ADD_USERS_DATA:'ADD_USERS_DATA',
-    ADD_CURRENT_USER:'ADD_CURRENT_USER'
+const usersDataActionTypes = {
+  ADD_USERS_DATA: "ADD_USERS_DATA",
+  ADD_CURRENT_USER: "ADD_CURRENT_USER",
+  ADD_SEARCH: "ADD_SEARCH"
 };
 
 export default usersDataActionTypes;
